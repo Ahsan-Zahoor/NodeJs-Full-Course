6bfe3c49ab2d36534445daadf1b4ab3fab705656
Ahsan-Zahoor <ahsanzahoor010@gail.com>
2024-03-01 03:12:26 +0500

    User model added to the register controller

---
+++ 14-Mongoose Data Models/model/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  roles: {
+    // User: {
+    //   type: Number,
+    //   default: 2001,
+    // }, //because its required every entry will have this value atlast so following this syntax, other single value means not required
+    User: Number,
+    Editor: Number,
+    Admin: Number,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  refreshTokens: String,
+});
+
+module.exports = mongoose.model("User", userSchema);
